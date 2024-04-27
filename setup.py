165ead86cbaba92e5190ddeb91adc98810b17525
Matthew Batchelder <borkweb@gmail.com>
2024-04-27 02:35:45 -0400

    Add pyproject.toml

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='pypipeline',
     packages=find_packages(),
-    version='0.1.0',
+    version='1.0.0',
     description='A library that implements the Chain of Responsibility pattern.',
     author='Matthew Batchelder',
     author_email='borkweb@gmail.com',
