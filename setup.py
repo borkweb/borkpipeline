ea2d0796f7e71c75015e0b9fde88f4010ed926f9
Matthew Batchelder <borkweb@gmail.com>
2024-04-27 08:58:27 -0400

    Updating project version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='pipelinechain',
     packages=find_packages(),
-    version='1.0.0',
+    version='1.0.1',
     description='A library that implements the Chain of Responsibility pattern.',
     author='Matthew Batchelder',
     author_email='borkweb@gmail.com',
